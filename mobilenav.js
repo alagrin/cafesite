be40d44190b8f38bad4ebf6865fa14bb7713123f
alagrin92 <alagrin92@gmail.com>
2018-04-05 18:02:13 -0700

    added js

--- mobilenav.js
+++ mobilenav.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.nav-wrapper').css("background-color", "#000099");
     $('nav').css("position", "fixed");
+
+    $('#intro').mouseenter(function() {
+      $(this).css("opacity", "0.8");
+    });
+    $('#intro').mouseleave(function() {
+      $(this).css("opacity", "0.5");
+    });
   });
